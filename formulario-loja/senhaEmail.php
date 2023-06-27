65dad185d2eb6a1c251cf4aba63b7cc939115b22
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2023-06-27 13:43:27 -0300

    Update cod

--- formulario-loja/senhaEmail.php
+++ formulario-loja/senhaEmail.php
@@ -1,4 +1,4 @@
 <?php
-    define('USER', 'ericksbarauna36@outlook.com');  // E-mail de quem vai enviar a Senha
-    define('PWD', 'erick12896347'); // Senha do E-mail de quem vai envair a senha
+    define('USER', 'email aqui');  // E-mail de quem vai enviar a Senha
+    define('PWD', 'senha aqui'); // Senha do E-mail de quem vai envair a senha
 ?>
